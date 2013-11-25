46fb7dc8096a019d55afb1273f9ccb312eb7aaa2
Joseph Malone <malone.joseph@gmail.com>
2013-11-24 19:58:34 -0800

    cleanup and adview

--- src/com/techventus/locations/LocationMap.java
+++ src/com/techventus/locations/LocationMap.java
@@ -1,44 +1,22 @@
 package com.techventus.locations;
 
-//import java.util.ArrayList;
 import java.util.List;
-
-//import android.app.Activity;
-//import android.content.Context;
-//import android.content.ComponentName;
-//import android.content.Context;
-//import android.content.Intent;
-//import android.content.ServiceConnection;
-//import android.content.pm.ActivityInfo;
-//import android.database.Cursor;
-//import android.database.sqlite.SQLiteDatabase;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-//import android.location.Location;
-////import android.location.LocationListener;
-//import android.location.LocationManager;
-
 import android.os.Bundle;
-//import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-//import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
-//import android.widget.RelativeLayout;
-//import android.widget.ZoomControls;
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.OverlayItem;
-//import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
-
-//import com.google.android.maps.OverlayItem;
 
 public class LocationMap extends MapActivity {
 
@@ -57,21 +35,26 @@ public class LocationMap extends MapActivity {
 	HelloItemizedOverlay itemizedOverlay;
 	MyLocationOverlay myLocation;
 	 GeoPoint point = new GeoPoint(37421791,-122084006);
-	//GeoPoint point = new GeoPoint(0, 0);
 
 	GeoPoint centrepoint = new GeoPoint(39421791, -98084006);
 	private MapController mapController;
 	protected MapView mapView;
 
-	// private LocationManager locationManager;
+    AdView mAdView;
 
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 
-		// setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT );
-		setContentView(R.layout.currentlocation); // bind the layout to the
-													// activity
+		setContentView(R.layout.currentlocation);
+
+        mAdView= (AdView)this.findViewById(R.id.ad);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("TEST_DEVICE_ID")
+                .build();
+        mAdView.loadAd(adRequest);
+
 		mapView = (MapView) findViewById(R.id.mapview);
 		myLocationButton = (Button) findViewById(R.id.thisLocation);
 		setButton = (Button) findViewById(R.id.mapSetButton);
@@ -139,29 +122,18 @@ public class LocationMap extends MapActivity {
 						+ t.getStackTrace()[0] + t.getStackTrace()[1]);
 			}
 
-			// myLocation.enableCompass();
-
-			Log.i("TECHVENTUS", "Before Map Overlay");
 			mapOverlays = mapView.getOverlays();
 			drawable = this.getResources().getDrawable(
 					R.drawable.mapmarkergreen);
-			Log.i("TECHVENTUS", "Resource Definition");
 			itemizedOverlay = new HelloItemizedOverlay(drawable, this);
-			Log.i("TECHVENTUS", "Done with BasisOverlay Creation");
 
 			 OverlayItem overlayitem = new OverlayItem(point, "myLocation",
 			 "myLocation");
 
-			// BasisOverlay bo = new BasisOverlay(itemizedOverlay);
-			Log.i("TECHVENTUS", "Done with OverlayItem");
 			 itemizedOverlay.addOverlay(overlayitem);
 
-			// Overlay g = new Overlay();
-			// mapOverlays.add(bo);
 			mapOverlays.add(itemizedOverlay);
-			// mapOverlays.
 			mapOverlays.add(myLocation);
-			Log.i("TECHVENTUS", "Done adding Overlay");
 
 			myLocationButton.setOnClickListener(new OnClickListener() {
 				public void onClick(View arg0) {
@@ -223,7 +195,6 @@ public class LocationMap extends MapActivity {
 	OnClickListener cancelClick = new OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			Log.e("TECHVENTUS", "CANCEL CLICK");
 			myLocation.disableCompass();
 			myLocation.disableMyLocation();
 			finish();
@@ -243,12 +214,6 @@ public class LocationMap extends MapActivity {
 		return false;
 	}
 
-	void fireErrorToast() {
-		Toast.makeText(
-				this,
-				"Error in Geopoint\n\rReselect Point.\n\rContact Developer if Error Persists",
-				Toast.LENGTH_LONG);
-	}
 
 	@Override
 	public void onResume() {
@@ -258,140 +223,3 @@ public class LocationMap extends MapActivity {
 	}
 
 }
-
-/*
- * try{
- * 
- * locationManager = (LocationManager)
- * getSystemService(Context.LOCATION_SERVICE);
- * 
- * Location l
- * =locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER); int lat
- * = (int) (l.getLatitude() * 1E6); int lng = (int) (l.getLongitude() * 1E6);
- * System.out.println(lat+" "+lng); GeoPoint point = new GeoPoint(lat, lng);
- * mapController.setCenter(point); }catch(Exception r){ Log.e("Joe",
- * "Location exception"+r.getLocalizedMessage()); }
- */
-/*
- * locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000,
- * 0, new GeoUpdateHandler());
- */
-
-/*
- * private void locClick(){ Intent i = new Intent(LocationMap.this,
- * LocationsMenu.class);
- * 
- * Log.w("TECHVENTUS", "Starting Activity"); LocationMap.this.startActivity(i);
- * }
- */
-
-// @Override
-// public void onPause(){
-// try{
-// unbindService(mConnection);
-// }catch(Exception e){
-// e.printStackTrace();
-// }
-// super.onPause();
-// }
-//
-// @Override
-// public void onDestroy(){
-// try{
-// unbindService(mConnection);
-// }catch(Exception e){
-// e.printStackTrace();
-// }
-// super.onDestroy();
-// }
-
-/*
- * private String[] getStringArSQLVertical(String query, SQLiteDatabase sql){
- * String[] ret = new String[0]; try{ Cursor c = sql.rawQuery(query, null);
- * List<String> list = new ArrayList<String>(); if(c!=null){
- * while(c.moveToNext()){ list.add(c.getString(0));
- * Log.e("TECHVENTUS","GET STRING AR"+c.getString(0)); //c.moveToNext(); } }
- * 
- * 
- * if(list.size()>0){ ret = list.toArray(new String[list.size()]);
- * Log.e("TECHVENTUS","GET STRING AR List size"+list.size()); //ret = new
- * String[list.size()]; //for(int i=0;i<list.size();i++){ // ret[i] =
- * list.get(i); //} } //c.getString(0); c.close(); }catch(Exception o){
- * 
- * o.printStackTrace(); } return ret; }
- */
-
-/*
- * OnClickListener setClick = new OnClickListener(){
- * 
- * @Override public void onClick(View arg0) {
- * Log.e("TECHVENTUS","Starting SET CLICK FINISH MAP"); //SQL SQLiteDatabase sql
- * = openOrCreateDatabase("db",0,null); try{ if(isNewLocation){
- * 
- * // String sqlString =
- * "INSERT INTO LOCATIONS (locationName, locationLatitude, locationLongitude, locationRadius, enabled) VALUES "
- * + //
- * "('"+locationName+"',"+point.getLatitudeE6()+","+point.getLongitudeE6()+","
- * +radius+",'"+true+"');"; // System.out.println(sqlString); // //
- * sql.execSQL(sqlString); // // // // sqlString =
- * "SELECT phoneName FROM PHONE;"; // //
- * System.out.println("SELECT phoneName from PHONE"); //
- * 
- * 
- * // String[] phones =getStringArSQLVertical(sqlString,sql); //
- * if(phones.length>0){ //
- * Log.e("TECHVENTUSPHONE","PHONES LENGTH "+phones.length); // for(int
- * i=0;i<phones.length;i++){ // System.out.println(
- * "INSERT INTO LOCATIONPHONEENABLE (locationName,phoneName,phoneEnable) VALUES ('"
- * +locationName+"','"+phones[i]+"',-"+2+");"); // sql.execSQL(
- * "INSERT INTO LOCATIONPHONEENABLE (locationName,phoneName,phoneEnable) VALUES ('"
- * +locationName+"','"+phones[i]+"',-"+2+");"); // // } // } //
- * sql.execSQL("INSERT INTO COMMAND (command) VALUES ('rectify');"); // }else{
- * 
- * // // String sqlString =
- * "UPDATE LOCATIONS SET locationLatitude = "+point.getLatitudeE6
- * ()+" , locationLongitude = "
- * +point.getLongitudeE6()+" where locationName = '"+locationName+"';"; //
- * System.out.println(sqlString); // sql.execSQL(sqlString); } sql.close();
- * }catch(Exception y){ Log.e("TECHVENTUS","Exception thrown");
- * y.printStackTrace(); }
- * 
- * 
- * // locationName VARCHAR, locationLatitude DOUBLE," // + " locationLongitude
- * DOUBLE, locationRadius INTEGER
- * 
- * 
- * if(isNewLocation){
- * 
- * if(point==null){ fireErrorToast(); }else{ Intent i = new
- * Intent(LocationMap.this,LocationDecisionMenu.class);
- * 
- * i.putExtra("locationName",locationName); i.putExtra("locationLatitude",
- * point.getLatitudeE6()); i.putExtra("locationLongitude",
- * point.getLongitudeE6()); i.putExtra("locationRadius", radius);
- * i.putExtra("locationEnabled", true); startActivity(i); } }
- * 
- * Log.e("TECHVENTUS","ABOUT TO FINISH MAP"); myLocation.disableCompass();
- * myLocation.disableMyLocation(); LocationMap.this.finish(); }
- * 
- * };
- */
-
-
-// GVLServiceInterface mIRemoteService;
-// private ServiceConnection mConnection = new ServiceConnection() {
-// // Called when the connection with the service is established
-// public void onServiceConnected(ComponentName className, IBinder service)
-// {
-// // Following the example above for an AIDL interface,
-// // this gets an instance of the IRemoteInterface, which we can use to
-// call on the service
-// mIRemoteService = GVLServiceInterface.Stub.asInterface(service);
-// }
-//
-// // Called when the connection with the service disconnects unexpectedly
-// public void onServiceDisconnected(ComponentName className) {
-// Log.e(TAG, "Service has unexpectedly disconnected");
-// mIRemoteService = null;
-// }
-// };
--- src/com/techventus/locations/NewLocation.java
+++ src/com/techventus/locations/NewLocation.java
@@ -2,20 +2,17 @@ package com.techventus.locations;
 
 
 import android.app.Activity;
-//import android.content.ComponentName;
-//import android.content.Context;
 import android.content.Intent;
-//import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
-//import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-//import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 
 public class NewLocation extends Activity{
@@ -26,30 +23,28 @@ public class NewLocation extends Activity{
 	 Button cancelButton;
 	 EditText locationBox;
 	 EditText metresBox;
-	 
+	AdView mAdView;
 
 
-	 
-	 
 	@Override protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        
-//	    Intent hello_service = new Intent(this, LocationService.class);
-//		bindService( hello_service, mConnection,Context.BIND_AUTO_CREATE);
 
         setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT ); 
         setContentView(R.layout.namenewlocation);
 
-       // LinearLayout layout = (LinearLayout) findViewById(R.id.namenewlocation);
+
+        mAdView= (AdView)this.findViewById(R.id.ad);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("TEST_DEVICE_ID")
+                .build();
+        mAdView.loadAd(adRequest);
+
         Log.e("TECHVENTUSPHONE","NEWLOCATION MENU - List Phone etc...");
-//        SQLiteDatabase sql =  openOrCreateDatabase("db",0,null);
-//        SQLHelper.listPhoneAndLocationPhoneEnable(sql);
-//        sql.close();
+
         
         saveButton = (Button)findViewById(R.id.NewLocationSaveButton);
         saveButton.setOnClickListener(saveButtonListener);
-        
-    	//ClickListener saveHandle = new ClickListener(){
         
         cancelButton= (Button)findViewById(R.id.NewLocationCancelButton);
         cancelButton.setOnClickListener(cancelButtonListener);
@@ -57,8 +52,6 @@ public class NewLocation extends Activity{
         
         locationBox = (EditText)findViewById(R.id.namenewlocationedittext);
         metresBox = (EditText)findViewById(R.id.newlocationmetresbox);
-        
-        
     	};
 	
     OnClickListener cancelButtonListener = new OnClickListener(){
@@ -108,62 +101,4 @@ public class NewLocation extends Activity{
 			}
 		}
 	};
-	
 }
-
-
-
-
-
-
-//
-//@Override 
-//public void onResume(){
-//	super.onResume();
-//	
-//    Intent hello_service = new Intent(this, LocationService.class);
-//    
-//	bindService( hello_service, mConnection,Context.BIND_AUTO_CREATE);
-//}
-
-//@Override
-//public void onPause(){
-//	try{
-//		unbindService(mConnection);
-//	}catch(Exception e){
-//		e.printStackTrace();
-//	}
-//	super.onPause();
-//}
-
-//@Override
-//public void onDestroy(){
-//	try{
-//		unbindService(mConnection);
-//	}catch(Exception e){
-//		e.printStackTrace();
-//	}
-//	super.onDestroy();
-//}		
-//
-
-
-
-
-//GVLServiceInterface mIRemoteService;
-//private ServiceConnection mConnection = new ServiceConnection() {
-//    // Called when the connection with the service is established
-//    public void onServiceConnected(ComponentName className, IBinder service) {
-//        // Following the example above for an AIDL interface,
-//        // this gets an instance of the IRemoteInterface, which we can use to call on the service
-//        mIRemoteService = GVLServiceInterface.Stub.asInterface(service);
-//    }
-//
-//    // Called when the connection with the service disconnects unexpectedly
-//    public void onServiceDisconnected(ComponentName className) {
-//        Log.e(TAG, "Service has unexpectedly disconnected");
-//        mIRemoteService = null;
-//    }
-//};
-
-
