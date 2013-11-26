c4e138b8dc154ff840ece7dc2764a3256525a4e7
Joseph Malone <malone.joseph@gmail.com>
2013-11-26 00:38:41 -0800

    new additions

--- src/com/techventus/locations/ReceiveTransitionsIntentService.java
+++ src/com/techventus/locations/ReceiveTransitionsIntentService.java
@@ -1,12 +1,13 @@
 package com.techventus.locations;
 
-import android.app.IntentService;
-import android.app.Service;
+import android.app.*;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.LocationClient;
+import com.techventus.server.voice.Voice;
+import com.techventus.server.voice.datatypes.AllSettings;
+import com.techventus.server.voice.datatypes.Phone;
 
 import java.util.List;
 
@@ -20,11 +21,15 @@ import java.util.List;
 public class ReceiveTransitionsIntentService extends IntentService
 {
 
+    private static final String TAG = ReceiveTransitionsIntentService.class.getSimpleName();
+
+    Settings mSettings;
     /**
      * Sets an identifier for the service
      */
     public ReceiveTransitionsIntentService() {
         super("ReceiveTransitionsIntentService");
+        mSettings = Settings.getInstance();
     }
     /**
      * Handles incoming intents
@@ -60,10 +65,27 @@ public class ReceiveTransitionsIntentService extends IntentService
             // Get the type of transition (entry or exit)
             int transitionType =
                     LocationClient.getGeofenceTransition(intent);
-
+            List<Geofence> list = LocationClient.getTriggeringGeofences(intent);
             Log.e("ReceiveTransitionsIntentService",
                     "Geofence transition error: " +
                             Integer.toString(transitionType));
+             String lst = "";
+            for(Geofence g:list)
+            {
+                lst+= g.getRequestId()+",";
+            }
+
+
+            {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Intent notificationIntent = new Intent(ReceiveTransitionsIntentService.this, MainMenu.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(ReceiveTransitionsIntentService.this, 0,notificationIntent, 0);
+                Notification notification = new Notification(R.drawable.ic_menu_compass, "New Message", System.currentTimeMillis());
+                notification.setLatestEventInfo(ReceiveTransitionsIntentService.this,lst+transitionType+"RT GVL", "Now: "+com.techventus.locations.Status.currentLocationString, pendingIntent);
+                notificationManager.notify(9999, notification);
+            }
+
+
 
             // Test that a valid transition was reported
             if ((transitionType == Geofence.GEOFENCE_TRANSITION_ENTER) || (transitionType == Geofence.GEOFENCE_TRANSITION_EXIT))
@@ -83,8 +105,58 @@ public class ReceiveTransitionsIntentService extends IntentService
                  */
             }
 
+
+        }
+}
+
+    /**
+     * Trigger location change.
+     */
+    synchronized void triggerLocationChange(){
+
+        if(Util.isNetworkConnected(this)){
+            try {
+                Voice voice = VoiceSingleton.getVoiceSingleton().getVoice();
+                AllSettings voiceSettings =voice.getSettings(false);
+                Phone[] phoneAr = voiceSettings.getPhones();
+                for(LPEPref lpe:mSettings.getPrefsList()){
+                    if(lpe.location.equals(Status.currentLocationString)){
+                        for(Phone phone:phoneAr){
+                            if(phone.getName().equals(lpe.phoneString)){
+                                System.out.println(""+phone.getName()+" "+lpe.enablePref);
+                                if(lpe.enablePref==1){
+                                    if(voiceSettings.isPhoneDisabled(phone.getId())){
+                                        voice.phoneEnable(phone.getId());
+                                        mSettings.setLocationChanged(true);
+                                    }
+
+                                }else if(lpe.enablePref==-1){
+                                    if(!voiceSettings.isPhoneDisabled(phone.getId())){
+                                        Log.e(TAG, "DISABLE "+phone.getId());
+                                        mSettings.setLocationChanged(true);
+                                        voice.phoneDisable(phone.getId());
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+                voice.getSettings(true);
+                mSettings.setReconnectToVoiceFlag(false);
+                mSettings.setPhoneUpdateFlag(false);
+            } catch (Exception e) {
+                mSettings.setReconnectToVoiceFlag(true);
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            mSettings.setPhoneUpdateFlag(true);
         }
     }
+
+
 
 
 //    /**
